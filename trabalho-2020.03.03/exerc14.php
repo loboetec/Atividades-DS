22aac1d51d4d7c31a02ab9951a272fb60e5e42c8
Guilherme <guilherme.lobo@etec.sp.gov.br>
2020-03-10 20:14:23 -0300

    atuacao de number_format

--- trabalho-2020.03.03/exerc14.php
+++ trabalho-2020.03.03/exerc14.php
@@ -2,13 +2,21 @@
 
 header ("Content-Type: text/html;charset=utf-8");
 
-$custo = 50;
-$venda = 65;
+$custo = 99100.64;
+$venda = 8565.99;
 
 $lucro = $venda - $custo; 
 
-echo "O lucro é = " . $lucro . " R$";
-
+if( $lucro > 0 ){
+    echo "O lucro é = R$ " . number_format( $lucro, 2, ',', '.' ) ;
+    exit ;
+}elseif( $lucro < 0 ){
+    echo "O prejuízo é = R$ " . number_format( $lucro, 2, ',', '.' ) ;
+    exit ;
+}else{
+    echo "Empatou o resultado = R$ " . number_format( $lucro, 2, ',', '.' ) ;
+    exit ;
+}
 
 exit ;
 ?>
